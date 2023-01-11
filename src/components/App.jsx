bff0d8d2899b33eb2c1e1290b66ce18d78e60f06
Ihor Smyk <ismyk256@gmail.com>
2023-01-12 00:32:51 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,13 @@ export const App = () => {
   const handleButtonClick = e => {
     switch (e.target.name) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prev => prev + 1);
         break;
       default:
     }
